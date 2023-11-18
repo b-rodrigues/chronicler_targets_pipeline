074765d2b719a632ae1ea03d334ed1cf8170ab4b
Bruno Rodrigues <brodriguesco@protonmail.com>
2023-11-18 14:32:08 +0000

    typo

--- _targets.R
+++ _targets.R
@@ -6,6 +6,7 @@ tar_option_set(packages = c("dplyr",
                             "tidyr",
                             "forcats",
                             "lubridate")
+                            )
 
 source("functions/functions.R")
 
